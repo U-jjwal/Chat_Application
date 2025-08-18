acd95049184830747992b52c9add2232a7ec465a
Ujjwal <ujjwaltiwari242@gmail.com>
2025-08-18 23:34:54 +0530

    Update socket.js

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -1,23 +1,16 @@
-import { io } from 'socket.io-client';
-
+import { io } from "socket.io-client";
 
 let socket = null;
 
 export const connectSocket = (userId) => {
-    socket = io(import.meta.env.MODE === "development" ? "https://chat-application-jxh8.vercel.app" : "/",
-        {  
-            query: { userId },
-        }
-    );
-    return socket;
-};
-
-export const getSocket = () => socket;
-
-export const disconnectSocket = () => {
-    if (socket) {
-        socket.disconnect();
-        socket = null;
+  socket = io(
+    import.meta.env.MODE === "production"
+      ? "https://chat-application-jxh8.vercel.app"  // 👈 backend on Vercel
+      : "http://localhost:4000",
+    {
+      query: { userId },
+      withCredentials: true,
     }
-}
-
+  );
+  return socket;
+};
