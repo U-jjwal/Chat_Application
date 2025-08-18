b5ceac315c609fd731f659ae8953e69598d8221b
Ujjwal <ujjwaltiwari242@gmail.com>
2025-08-18 22:52:47 +0530

    Update socket.js

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -4,7 +4,7 @@ import { io } from 'socket.io-client';
 let socket = null;
 
 export const connectSocket = (userId) => {
-    socket = io(import.meta.env.MODE === "development" ? "http://localhost:4000" : "/",
+    socket = io(import.meta.env.MODE === "development" ? "https://chat-application-jxh8.vercel.app/" : "/",
         {  
             query: { userId },
         }
