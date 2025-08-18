0b311d92259b80a5a8726cc5c9160876a50677ca
Ujjwal <ujjwaltiwari242@gmail.com>
2025-08-18 23:34:16 +0530

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://chat-application-jxh8.vercel.app/api/v1" : "/",
-    withCredentials: true,
+  baseURL: import.meta.env.MODE === "production"
+    ? "https://chat-application-jxh8.vercel.app/api/v1"   // 👈 backend on Vercel
+    : "http://localhost:4000/api/v1",                     // 👈 local dev
+  withCredentials: true,
 });
